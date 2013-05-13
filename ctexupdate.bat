afbd92fe890aa3a95d475e49a7ea562280839687
FireUponSky <fireuponsky@163.com>
2013-05-14 00:23:20 +0800

    perfect ctexupdate.

--- ctexupdate.bat
+++ ctexupdate.bat
@@ -1,6 +1,6 @@
 @echo off
 :: BatchGotAdmin  
-:-------------------------------------  
+::-------------------------------------  
 REM  --> Check for permissions  
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
   
@@ -21,32 +21,51 @@ if '%errorlevel%' NEQ '0' (
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
     pushd "%CD%"  
     CD /D "%~dp0"  
-:--------------------------------------  
+::--------------------------------------  
+set nStep=0
+:cleanTemp
+dir %temp%\mik* /a /b > %temp%\dirlist.txt 2>nul
+for /f %%i in (%temp%\dirlist.txt) do rd /S /Q %temp%\%%i
+del %temp%\dirlist.txt
+if %nStep%==1 goto 2edup
+if %nStep%==2 goto repairPack
+if %nStep%==3 goto DBRen
 
-rd /S /Q %temp% 1>nul 2>nul & md %temp% 1>nul 2>nul
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo     联网升级需要较长时间，请不要关闭本窗口！
 echo.
-echo        本文件请右键“管理员权限”运行！
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
-
 
 mpm --set-repository http://mirrors.ustc.edu.cn/CTAN/systems/win32/miktex/tm/packages/
 ::mpm --set-repository ftp://ftp.ctex.org/mirrors/CTAN/systems/win32/miktex/tm/packages/
 echo 源设置成功
 mpm --admin --update --verbose
 ping -n 10 127.0.0.1 1>nul 2>nul
+set nStep=1
+goto cleanTemp
+
+:2edup
 mpm --admin --update --verbose
 echo 升级完毕
+set nStep=2
+goto cleanTemp
+
+:repairPack
 mpm --admin --install=amslatex-primer
 mpm --admin --install=amsmath
 mpm --admin --install=amscls
 mpm --admin --install=tex-gyre-math
 echo 修复完毕
+set nStep=3
+goto cleanTemp
+
+
+:DBRen
 mpm --admin --update-db
 initexmf --admin --update-fndb
 echo 刷新数据库完毕
+
 
 pause

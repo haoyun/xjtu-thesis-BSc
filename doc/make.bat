b0ad06f2a48c8b45f340485ca956a92532586427
FireUponSky <fireuponsky@163.com>
2013-05-15 21:23:20 +0800

    修改make.bat为完整的用户接口。

--- doc/make.bat
+++ doc/make.bat
@@ -1,38 +1,58 @@
 @echo off
+::这个文件用作用户界面，就这么愉快的决定了~~
+set mainpath=%~dp0
 :init
-if /i {%1}=={all} goto makepdf
-if /i {%1}=={help} goto help
-@echo off
-:init
-if /i {%1}=={all} goto makepdf
-if /i {%1}=={help} goto help
-if /i {%1}=={clean} goto clean
-if /i {%1}=={} goto all
-goto help
-
-:all
-goto makepdf
+cd %mainpath%
+cls
+echo  ------------------------------
+echo          主人你来了~~~！！！
+echo  ------------------------------
+echo.
+echo         1、查看帮助文档
+echo         2、编译论文
+echo         3、我要走了，你自己玩吧%>_<%
+echo.
+echo  ------------------------------
+set /p choice= 动手吧( ⊙ o ⊙ )：
+if not "%choice%"=="" set choice=%choice:~0,1%
+if /i %choice%==1 goto help
+if /i %choice%==2 goto makepdf
+if /i %choice%==3 goto end
+echo 不要乱来！
 pause
-goto clean
-goto end
+echo.
+goto init
 
 :makepdf
-if not exist xjtubsc.cls goto clsmissing
-xelatex main.tex -synctex=1 -undump=xelatex --shell-escape
-bibtex example.tex
-xelatex main.tex -synctex=1 -undump=xelatex --shell-escape
-xelatex main.tex -synctex=1 -undump=xelatex --shell-escape
+xelatex main.tex
+bibtex  main.tex
+xelatex main.tex
+xelatex main.tex
+goto clean
 
 :help
-echo 有人卖拐请按1，有人卖车请按2, ......
+if not exist doc\main.pdf goto makehelp
+start doc\main.pdf
+goto init
+
+:makehelp
+cd doc
+xelatex main.tex
+bibtex  main.tex
+xelatex main.tex
+xelatex main.tex
+goto clean
+
 
 :clean
 del *.aux
 del *.log
 del *.out
 del *.toc
+goto read
 
-:clsmissing
-echo "丢失 cls"
+:read
+start main.pdf
+goto init
 
 :end
---
+++ make.bat
@@ -0,0 +1,58 @@
+@echo off
+::这个文件用作用户界面，就这么愉快的决定了~~
+set mainpath=%~dp0
+:init
+cd %mainpath%
+cls
+echo  ------------------------------
+echo          主人你来了~~~！！！
+echo  ------------------------------
+echo.
+echo         1、查看帮助文档
+echo         2、编译论文
+echo         3、我要走了，你自己玩吧%>_<%
+echo.
+echo  ------------------------------
+set /p choice= 动手吧( ⊙ o ⊙ )：
+if not "%choice%"=="" set choice=%choice:~0,1%
+if /i %choice%==1 goto help
+if /i %choice%==2 goto makepdf
+if /i %choice%==3 goto end
+echo 不要乱来！
+pause
+echo.
+goto init
+
+:makepdf
+xelatex main.tex
+bibtex  main.tex
+xelatex main.tex
+xelatex main.tex
+goto clean
+
+:help
+if not exist doc\main.pdf goto makehelp
+start doc\main.pdf
+goto init
+
+:makehelp
+cd doc
+xelatex main.tex
+bibtex  main.tex
+xelatex main.tex
+xelatex main.tex
+goto clean
+
+
+:clean
+del *.aux
+del *.log
+del *.out
+del *.toc
+goto read
+
+:read
+start main.pdf
+goto init
+
+:end

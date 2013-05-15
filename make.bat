f6fb4873e1225ea46e34c95388bbbe96e82b6569
FireUponSky <fireuponsky@163.com>
2013-05-15 21:30:35 +0800

    为make.bat添加升级ctex功能。

--- make.bat
+++ make.bat
@@ -4,20 +4,22 @@ set mainpath=%~dp0
 :init
 cd %mainpath%
 cls
-echo  ------------------------------
+echo  ----------------------------------------
 echo          主人你来了~~~！！！
-echo  ------------------------------
+echo  ----------------------------------------
 echo.
 echo         1、查看帮助文档
 echo         2、编译论文
-echo         3、我要走了，你自己玩吧%>_<%
+echo         3、升级CTEX
+echo         4、我要走了，你自己玩吧T_T
 echo.
-echo  ------------------------------
+echo  ----------------------------------------
 set /p choice= 动手吧( ⊙ o ⊙ )：
 if not "%choice%"=="" set choice=%choice:~0,1%
 if /i %choice%==1 goto help
 if /i %choice%==2 goto makepdf
-if /i %choice%==3 goto end
+if /i %choice%==3 goto updatectex
+if /i %choice%==4 goto end
 echo 不要乱来！
 pause
 echo.
@@ -53,6 +55,11 @@ goto read
 
 :read
 start main.pdf
+goto init
+
+
+:updatectex
+call ctexupdate.bat
 goto init
 
 :end
